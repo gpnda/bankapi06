83982015afd3ab89577de9e9d59ea0f995a41758
Lum <lumtech@yandex.ru>
2025-03-09 17:02:15 +0300

    connected to bank api

--- 1.py
+++ 1.py
@@ -7,5 +7,27 @@ import json
 
 gc = gspread.service_account(filename="./credentials.json")
 sh = gc.open("api2025")
+print("RESULT: ")
 print(sh.sheet1.acell('A1').value)
+
+
+
+
+
+tokenfile_path = './tinkoff-token.txt'
+with open(tokenfile_path, 'r', encoding='utf-8') as file:
+    tinkoff_token = file.read()
+
+import http.client
+conn = http.client.HTTPSConnection("business.tbank.ru")
+payload = ''
+headers = {
+  'Accept': 'application/json',
+  'Authorization': 'Bearer ' + tinkoff_token + ''
+}
+conn.request("GET", "/openapi/api/v1/statement?accountNumber=40802810000000840917&from=2022-02-01T21:00:00Z", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
+
 
